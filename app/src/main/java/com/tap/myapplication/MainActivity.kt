2630c72f37cc4303734ddece702f6aec55272d7f
aslm-android <130293799+aslm-android@users.noreply.github.com>
2023-09-06 09:31:41 -0300

    updated gradle version

--- app/src/main/java/com/tap/myapplication/MainActivity.kt
+++ app/src/main/java/com/tap/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tap.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.tap.commondatamodels.currencies.GlobalCurrency
 
 
 class MainActivity : AppCompatActivity() {
